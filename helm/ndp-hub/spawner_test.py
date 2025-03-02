6d8a52cf0a759b844b4ffa1cb90f1cc82f7a0303
alexandertuna <alexander.tuna@gmail.com>
2025-03-01 17:11:03 -0800

    Update spawner_test.py to bump version (#19)

--- helm/ndp-hub/spawner_test.py
+++ helm/ndp-hub/spawner_test.py
@@ -34,7 +34,7 @@ def use_k8s_secret(namespace, secret_name):
 NAMESPACE = 'ndp-test'
 CLIENT_ID, CLIENT_SECRET = use_k8s_secret(namespace=NAMESPACE, secret_name='jupyterhub-secret')
 KEYCLOAK_URL = "https://idp-test.nationaldataplatform.org"
-NDP_EXT_VERSION = '0.0.8'
+NDP_EXT_VERSION = '0.0.9'
 
 USER_PERSISTENT_STORAGE_FOLDER = "_User-Persistent-Storage_CephBlock_"
 
