825504aa7a3caf11752464a0e67ac349cf3e4303
Siddharth Trivedi <sstrivedi@ucsd.edu>
2026-04-27 23:14:12 -0700

    add vscode+rstudio to default minimal ndp image

--- helm/ndp-hub/spawner_test.py
+++ helm/ndp-hub/spawner_test.py
@@ -54,24 +54,6 @@ original_profile_list = [
         'slug': "1",
     },
     {
-        'display_name': "Minimal NDP Starter Jupyter Lab + RStudio",
-        'slug': "11",
-        'default': False,
-        'kubespawner_override': {
-            'image': 'gitlab-registry.nrp-nautilus.io/ndp/ndp-docker-images/jhub-spawn:rstudio_v0.0.1',
-            'default_url': '/lab'
-        }
-    },
-    {
-        'display_name': "Minimal NDP Starter Jupyter Lab + RStudio + VSCode",
-        'slug': "12",
-        'default': False,
-        'kubespawner_override': {
-            'image': 'gitlab-registry.nrp-nautilus.io/ndp/ndp-docker-images/jhub-spawn:rstudio_vscode_v0.0.6',
-            'default_url': '/lab'
-        }
-    },
-    {
         'display_name': "NDP Catalog Search",
         'default': False,
         'slug': "10",
